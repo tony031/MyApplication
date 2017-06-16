c1d28257dd7a08afa6315e579fa3778ed5d0514c
tony031 <ecyber2k@gmail.com>
2017-06-16 10:22:28 +0800

    1st Change

--- app/src/main/java/com/golfmechanix/myapplication/MainActivity.java
+++ app/src/main/java/com/golfmechanix/myapplication/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    // TODO: 2017/06/16 1st change
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
